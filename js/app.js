bf8419f107ea5cc92f9793e456b63683eb934749
jesusgabriel <amelo@fvi.edu>
2016-09-08 18:31:17 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//Problem: User when clicking on image goes to a dead end
+//Solution: Create an overlay with the large image - Lightbox
+var overlay = $("<div id='overlay'></div>")
+var img = $("<img/>")
+var caption = $("<p></p>")
+
+overlay.append(img)
+overlay.append(caption)
+console.log(overlay);
+$("body").append(overlay)
+overlay.hide()
+
+
+$('a').click(function(event){
+event.preventDefault()
+overlay.show()
+var imageLocation = $(this).attr("href")
+img.attr('src',imageLocation )
+})
+overlay.click(function(){
+  overlay.hide()
+})
